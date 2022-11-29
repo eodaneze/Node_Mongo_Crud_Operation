57c3fe46fcf4abfaf09d42e3196c9b95ba20be47
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-11-29 06:08:06 -0800

    creating usermodels and sending an async request to the database

---
+++ models/userModels.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+})
+
+module.exports = mongoose.model('users', userSchema)
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,20 @@
+const { Router } = require('express');
 const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("API IS RUNNING")
-    console.log("API IS RUNNING");
+const User = require('../models/userModels')
+
+router.get('/', async(req, res) => {
+    try{
+        const users = await User.find();
+        res.json(users)
+    }catch(err){
+        res.status(404).send("ERROR OCCURED")
+    }
 })
 
-module.exports = router
+
+
+
+module.exports = router;
